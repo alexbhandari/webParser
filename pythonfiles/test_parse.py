b2f126740be7de10c567311219dff9c8bb88f90c
alexbhandari <alexbhandari@gmail.com>
2013-09-27 11:46:20 -0400

    improved print, do we need to sort words by count?

--- pythonfiles/test_parse.py
+++ pythonfiles/test_parse.py
@@ -3,7 +3,17 @@ import parse
 link = ['http://www.nytimes.com']
 parse_object = parse.parse(link)
 parse_object.parse()
-print('WORDS:')
-pprint(parse_object.get_words())
-print('Filtered:')
-pprint(parse_object.get_filtered())
+
+def pdict(d,f):
+   for i,c in d.items():
+      print(f.format(str(i),str(c)))
+
+print('{:<20} {:<20}'.format('Word','Count'))
+pdict(parse_object.get_words(),'{:<20} {:<20}')
+print()
+print('{:<20} {:<20}'.format('Filtered','Count'))
+pdict(parse_object.get_filtered(),'{:<20} {:<20}')
+
+
+#pprint(parse_object.get_words())
+#pprint(parse_object.get_filtered())
