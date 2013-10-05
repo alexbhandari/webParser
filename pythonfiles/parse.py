601cc90bf7122dd90fea694279d27d134d8af4f2
alexbhandari <alexbhandari@gmail.com>
2013-10-05 00:02:44 -0400

    implemented print method. Updated parse object structure

--- pythonfiles/parse.py
+++ pythonfiles/parse.py
@@ -3,6 +3,7 @@
 import urllib.request
 from bs4 import BeautifulSoup
 from collections import Counter
+from word import word
 #import operator
 import re
 
@@ -16,7 +17,7 @@ class parse(object):
       return self.words
    def get_filtered(self):
       return self.filtered
-#   def print(self)
+#  def print(self) --BELOW--
    
    #outputs strings in words[] list matching input regex
    def test_regex(words,regex):
@@ -31,12 +32,9 @@ class parse(object):
                filtered.append(y.group(0))
       count = Counter(filtered)
       return count
-   #def print_parse(wcount,filtered):
-   #   print('Words: ' + wcount)
-   #   print('Filtered: ' + filtered)
-   
-   def parse(self):
 
+   #get words from links, count and store in word list
+   def parse(self):
       words = []
       for link in self.links:
          #get html
@@ -61,10 +59,38 @@ class parse(object):
       #this should be made into a method.
       words[:] = [re.sub(regex,'',x) for x in words]
       #returns a dictionary object
-      self.words = Counter(words)
-      
-      #switched to dict_items
-      #count_items = count.items()
-      
-      #switched to list: sorted returns list
-      #sorted_words = list(reversed(sorted(count_items,key=operator.itemgetter(1))))
+      words = Counter(words)
+      for x,y in words.items():
+         #could also use dict defaultset()
+         if x in self.words:
+            self.words[x].incr_count(y) 
+         else:
+            self.words[x] = word(x,None,None,None,None,y,None)
+
+def print(self):
+   #create list of words sorted in decending order
+   sorted_words = list(reversed(sorted(list(self.words.values()), key=lambda x: x.get_count())))
+   #create a list of touples (filtered symbol, count) --SHOULD CHANGE TO OBJECT--
+   sorted_filtered = list(reversed(sorted(self.filtered().items(),key=operator.itemgetter(1))))
+   #prints a list of touples 
+   def p_touple_list(l,f):
+      i=0
+      for k,c in l:
+         print(f.format(str(k),c))
+         i = i+1
+      print('Number of elements is ' + str(i))
+   #prints a list of words
+   def p_word_list(l,f):
+      i=0
+      for w in l:
+         print(f.format(w.get_name(),w.get_count()))
+         i = i+1
+      print('Number of elements is ' + str(i))
+   
+   print('{:<20} {:<20}'.format('Word','Count'))
+   print()
+   p_word_list(sorted_words,'{:<20} {:5d}')
+   print()
+   print('{:<20} {:<20}'.format('Filtered','Count'))
+   print()
+   p_touple_list(sorted_filtered,'{:<20} {:5d}')
--- pythonfiles/test_parse.py
+++ pythonfiles/test_parse.py
@@ -1,24 +1,36 @@
-from pprint import pprint
+#from pprint import pprint
 import operator
 import parse
+#link = ['http://www.nytimes.com']
 link = ['http://www.nytimes.com']
 parse_object = parse.parse(link)
 parse_object.parse()
 
-sorted_words = list(reversed(sorted(parse_object.get_words().items(),key=operator.itemgetter(1))))
+#sort = parse_object.get_words().values()
+sorted_words = list(reversed(sorted(list(parse_object.get_words().values()), key=lambda x: x.get_count())))
 sorted_filtered = list(reversed(sorted(parse_object.get_filtered().items(),key=operator.itemgetter(1))))
 
-def plist(d,f):
-   for i,c in d:
-      print(f.format(str(i),str(c)))
+def plist(l,f):
+   i=0
+   for k,c in l:
+      print(f.format(str(k),c))
+      i = i+1
+   print('Number of elements is ' + str(i))
+def pwlist(l,f):
+   i=0
+   for w in l:
+      print(f.format(w.get_name(),w.get_count()))
+      i = i+1
+   print('Number of elements is ' + str(i))
 
 print('{:<20} {:<20}'.format('Word','Count'))
+print()
 #pdict(parse_object.get_words(),'{:<20} {:<20}')
-plist(sorted_words,'{:<20} {:<20}')
+pwlist(sorted_words,'{:<20} {:5d}')
 print()
 print('{:<20} {:<20}'.format('Filtered','Count'))
 #pdict(parse_object.get_filtered(),'{:<20} {:<20}')
-plist(sorted_filtered,'{:<20} {:<20}')
+plist(sorted_filtered,'{:<20} {:5d}')
 
 
 #pprint(parse_object.get_words())
