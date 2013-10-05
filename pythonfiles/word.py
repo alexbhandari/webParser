3b41e3bcc721d47ce5a7314af2ac4d9006598164
alexbhandari <alexbhandari@gmail.com>
2013-10-05 00:04:26 -0400

    add word class

---
+++ pythonfiles/word.py
@@ -0,0 +1,38 @@
+
+#Comtains models for storing words and data returned from parsing websites
+#
+class word(object):
+   def __init__(self,name=None,part_of_speech=None,definition=None,context=None,subwords=None,count=None,rating=None):
+      self.name=name
+      self.part_of_speech=part_of_speech
+      self.definition=definition
+      self.context=context
+
+      self.subwords=subwords
+      
+      self.count=count
+      self.rating=rating
+  
+   def __str__(self):
+      return self.name
+   def get_name(self):
+      return self.name
+   def get_part_of_speech(self):
+      return self.part_of_speech
+   def get_definition(self):
+      return self.definition
+   def get_context(self):
+      return self.context
+   
+   def get_forms(self):
+      return self.forms
+   
+   def get_count(self):
+      return self.count
+   def get_rating(self):
+      return self.rating
+
+   def set_count(self,value):
+      self.count = value
+   def incr_count(self,value):
+      self.count += value
